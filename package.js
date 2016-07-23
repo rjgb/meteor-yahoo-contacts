69b395aac05e3313c347b7a1ebb62c4cfd69fae4
Brian Glass <glass@manyhandscollective.com>
2016-07-22 21:11:12 -0500

    initial commit

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'rjgb:yahoo-contacts',
+  version: '0.0.1',
+  summary: 'Yahoo Contacts package',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
+  api.addFiles('index.js', 'server');
+  api.export('YahooContacts');
+});
+
+Npm.depends({
+  'underscore': '1.5.2'
+});
